3b81a2a76bfed46963daa8dc8f15353d2f0c2481
CARLOS ALBERTO BRANDELERO <mastermil@msn.com>
2023-09-16 19:49:54 -0300

    Movimento de entidades

--- MountainShooter/code/EntityFactory.py
+++ MountainShooter/code/EntityFactory.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
 from code.Background import Background
-from code.Const import WIN_WIDTH
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Enemy import Enemy
+from code.Player import Player
 
 
 class EntityFactory:
@@ -14,3 +17,11 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+            case 'Player1':
+                return Player('Player1', (10, 330 - WIN_HEIGHT / 2))
+            case 'Player2':
+                return Player('Player2', (10, 30 + WIN_HEIGHT / 2))
+            case 'Enemy1':
+                return Enemy('Enemy1', (WIN_WIDTH + 40, random.randint(0, 324)))
+            case 'Enemy2':
+                return Enemy('Enemy2', (WIN_WIDTH + 40, random.randint(0, 324)))
--- MountainShooter/code/Player.py
+++ MountainShooter/code/Player.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+import pygame
 
-from Entity import Entity
+from code.Const import WIN_HEIGHT, ENTITY_SPEED, WIN_WIDTH, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_RIGHT, \
+    PLAYER_KEY_LEFT
+from code.Entity import Entity
+
 
 class Player(Entity):
-    def __init__(self):
-        pass
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
-        pass
+        pressed_key = pygame.key.get_pressed()
+        if pressed_key[PLAYER_KEY_UP[self.name]] and self.rect.top > 0:  # Quando seta pra cima faça
+            self.rect.centery -= ENTITY_SPEED[self.name]
 
+        if pressed_key[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_HEIGHT:  # Quando seta pra baixo faça
+            self.rect.centery += ENTITY_SPEED[self.name]
+
+        if pressed_key[PLAYER_KEY_LEFT[self.name]] and self.rect.left > 0:  # Quando seta pra frente faça
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+
+        if pressed_key[PLAYER_KEY_RIGHT[self.name]] and self.rect.right < WIN_WIDTH:  # Quando seta pra traz faça
+            self.rect.centerx += ENTITY_SPEED[self.name]
